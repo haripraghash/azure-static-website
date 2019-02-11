0771f90f5840794da90f21cd7ed159138bf9fb68
Haripraghash Subramaniam <harioverhere@hotmail.com>
2019-02-11 15:46:54 +0000

    commit

--- azurestaticwebsite/AzureStaticWeb.Provisioning/Deploy-AzureResourceGroup.ps1
+++ azurestaticwebsite/AzureStaticWeb.Provisioning/Deploy-AzureResourceGroup.ps1
@@ -95,7 +95,7 @@ $deployment
 $context = Get-AzureRmContext
 $cache = $context.TokenCache
 $cacheItem = $cache.ReadItems()
-$token = ($cacheItem | where { $_.Resource -eq "https://graph.windows.net/" }).accessToken
+$token = $cacheItem.accessToken
 $token
 if ($ValidateOnly) {
     $ErrorMessages = Format-ValidationOutput (Test-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
